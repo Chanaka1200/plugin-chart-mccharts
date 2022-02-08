d5d9e0f3c75cccb54d97a9457fd516049b3af185
chanaka1200 <chanaka.bandara00@gmail.com>
2022-02-08 05:47:18 -0500

    first commit

---
+++ lib/Mccharts.d.ts
@@ -0,0 +1,11 @@
+import { McchartsProps } from './types';
+import 'react-tabs/style/react-tabs.css';
+/**
+ * ******************* WHAT YOU CAN BUILD HERE *******************
+ *  In essence, a chart is given a few key ingredients to work with:
+ *  * Data: provided via `props.data`
+ *  * A DOM element
+ *  * FormData (your controls!) provided as props by transformProps.ts
+ */
+export default function Mccharts(props: McchartsProps): JSX.Element;
+//# sourceMappingURL=Mccharts.d.ts.map
